999b749d8d9692221d24321ac23b16df185a826b
Vanya Shevchuk <aggressor2000@gmail.com>
2020-04-16 18:49:04 +0300

    Updates

---
+++ precache-manifest.5797298dfe1ddb5b6443a08575007763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc21bad6836b1d82d72d54698b469886",
+    "url": "/React-To-do-app/index.html"
+  },
+  {
+    "revision": "e74a80f46ef3bcc55a7c",
+    "url": "/React-To-do-app/static/css/main.d90d6352.chunk.css"
+  },
+  {
+    "revision": "b3d0295b04ae5e7de3f3",
+    "url": "/React-To-do-app/static/js/2.bd9475bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-do-app/static/js/2.bd9475bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74a80f46ef3bcc55a7c",
+    "url": "/React-To-do-app/static/js/main.3680b1a4.chunk.js"
+  },
+  {
+    "revision": "067b291e921b4e596be7",
+    "url": "/React-To-do-app/static/js/runtime-main.10f5ac4f.js"
+  }
+]);
